48c6de556ef32beaaa82c826dec47e42f5547ecd
Benjamin Bearce <bbearce@gmail.com>
2024-01-10 15:03:53 -0700

    init

---
+++ IVIM/sample_submission/retrieve.sh
@@ -0,0 +1,4 @@
+# Store or backup
+cp -r /sddata/projects/Challenges/IVIM/sample_submission/*.npy /sddata/data/Challenges/IVIM/sample_submission/
+# Get
+cp -r /sddata/data/Challenges/IVIM/sample_submission/*.npy /sddata/projects/Challenges/IVIM/sample_submission/
---
+++ IVIM/sample_submission_from_yan/retrieve.sh
@@ -0,0 +1,5 @@
+# Store or backup
+cp -r /sddata/projects/Challenges/IVIM/sample_submission_from_yan/*.npy /sddata/data/Challenges/IVIM/sample_submission_from_yan/
+cp -r /sddata/projects/Challenges/IVIM/sample_submission_from_yan/*.py /sddata/data/Challenges/IVIM/sample_submission_from_yan/
+# Get
+cp -r /sddata/data/Challenges/IVIM/sample_submission_from_yan/*.npy /sddata/projects/Challenges/IVIM/sample_submission_from_yan/
